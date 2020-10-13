10221e8083edb12497cb16c53cf71861027fcde5
Alex Serafini <alexserafini2001@gmail.com>
2020-10-12 21:23:58 -0400

    Ya tiene el tokenize

--- Program.cs
+++ Program.cs
@@ -4,11 +4,32 @@ namespace ReversePolishNotation
 {
     class Program
     {
-        public string valor;
         static void Main(string[] args)
         {
-            Console.WriteLine("Bienvenido al validador de formulas proposicionales");
-            valor = Console.ReadLine();
+            int entrada = 0;
+            string valor;
+            string[] pruebas = { "", "", "", "", "", "" };
+            Random rng = new Random();
+            while(true)
+            {
+                Console.WriteLine("Bienvenido al validador de formulas proposicionales");
+                Console.WriteLine("1. Casos de Prueba");
+                Console.WriteLine("2. Insertar una formula proposicional");
+                Console.WriteLine("3. terminar sesion");
+                if  (entrada == 1)
+                {
+                    rng.Next(0, 7);
+                }
+                if  (entrada == 2)
+                {
+                    valor = Console.ReadLine();
+                    Console.WriteLine(valor.ToLower());
+                }
+                if  (entrada == 3) 
+                {
+                    break;
+                }
+            }
         }
     }
 }
--- ReversePolishNotation.cs
+++ ReversePolishNotation.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualBasic;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Net;
 using System.Text;
 
@@ -14,12 +16,20 @@ namespace ReversePolishNotation
     }
     class ReversePolishNotation
     {
+        //Esta funcion determina si la entrada de tanto el usuario como los casos de prueba son validos para ser tokenizados y resolvidos
         public bool getInput(string expresion)
         {
-            expresion.Trim().Replace(" ", "").Replace("(", "").Replace(")", "");
-            foreach(char i in expresion)
+            for(int i = 0; i < expresion.Length; i++)
             {
-                if (i != logicalOperators.AND || i != logicalOperators.OR || i != logicalOperators.CON || !Char.IsLower(i)) return false;
+                if ( expresion[i] == ' ' && expresion[i + 1] == ' ' ||
+                     expresion[i] != logicalOperators.AND &&
+                     expresion[i] != logicalOperators.OR && 
+                     expresion[i] != logicalOperators.CON &&
+                     (expresion[i] > 'a' && expresion[i] < 'z') &&
+                     !Char.IsLower(expresion[i]))
+                {
+                    return false;
+                }
             }
             return true;
         }
@@ -29,19 +39,18 @@ namespace ReversePolishNotation
          y los va acumulando en un arreglo de cadenas que se da como salida 
          conjuntamente con la longitud del mismo
          */
-        public void tokenizeinput(string postFixExp, string[] splitStr, int inputLength)
-        {
-
-        }
+        public void tokenizeinput(string postFixExp, string[] splitStr) => splitStr = postFixExp.Split(" ");
         /*
          Esta función toma como entrada un arreglo de cadenas y la longitud
          del mismo. Este debe contener los componentes (operandos y operadores)
          que componen la expresión postfija en el mismo orden en que fueron detectados.
          La función deberá usar una pila para computar determinar si se trata o no
          de una formula bien formada en notacio'n polaca inversa.
-         */
-        public int parseFormula(string[] splitStr, int inputLength)
+        */
+        public int parseFormula(string[] splitStr)
         {
+            Stack wff = new Stack();
+
             return 0;
         }
     }
